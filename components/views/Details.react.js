51552f8f7896717e0113f3f0928b455ddca4e609
lawal lanre <lanre.lawal@andela.com>
2019-04-10 09:57:36 +0100

    implement details page dial, website and directions actions

--- components/views/Details.react.js
+++ components/views/Details.react.js
@@ -1,140 +1,171 @@
 import React from 'react'
-import {ScrollView, View, ImageBackground, Text, Image} from 'react-native'
+import {ScrollView, View, ImageBackground, Text, Image, Linking, Platform} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import PropTypes from 'prop-types'
 import TouchAble from './TouchAble.react'
 import Ratings from './Rating.react'
 import Divider from './Divider.react'
 import restaurantCard from '../../assets/styles/restaurantCard'
 import trimText from '../../utils/trimText'
+import details, {flexCenter, centerAlign} from '../../assets/styles/details'
+import {marginLeftAuto} from '../../assets/styles/shared'
+
+const openLink = url => {
+  if (url) {
+    Linking.canOpenURL(url)
+      .then(supported => {
+        if (!supported)
+          console.error("Can't handle url: " + url)
+        else
+          return Linking.openURL(url)
+      })
+      .catch((err) => console.error('An error occurred', err))
+  }
+}
 
 const Details = props => {
-  const {image_url, distance, isFav, categories, rating, name, is_closed} = props.data
+  const {
+    image_url, distance, isFav, categories, rating, name, is_closed, phone, url, coordinates
+  } = props.data
+  const {latitude = 0, longitude = 0} = coordinates
 
   return (
-    <View style={{flex: 1}}>
+    <View style={details.container}>
       <ScrollView
-        style={{flex: 1}}
+        style={details.container}
         showsVerticalScrollIndicator={false}>
         <ImageBackground
           source={{uri: image_url}}
-          style={{
-            height: 250,
-            flexDirection: 'row',
-            padding: 20,
-            paddingTop: 30,
-            justifyContent: 'space-between',
-            backgroundColor: 'gray'
-          }}>
+          style={details.banner}>
           <TouchAble onPress={() => props.navigation.goBack()}>
             <Icon name='md-arrow-back' size={25} color='white'/>
           </TouchAble>
           <Icon
-            style={{}}
             name={isFav ? 'ios-heart' : 'ios-heart-empty'}
             size={25}
             color='white'
           />
         </ImageBackground>
-        <View style={{padding: 20, justifyContent: 'center'}}>
+        <View style={details.actionsRow}>
           <View
-            style={{flexDirection: 'row', paddingTop: 10, paddingBottom: 20, justifyContent: 'space-between', alignItems: 'center'}}>
+            style={details.firstRow}>
             <Image
               source={{uri: image_url}}
-              style={{width: 60, height: 60, borderRadius: 60}}
+              style={details.imageCircle}
             />
             <View>
               <View style={restaurantCard.captionHeading}>
                 <Text style={restaurantCard.main}>{trimText(name)}</Text>
               </View>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <View style={flexCenter}>
                 <Ratings rating={rating} />
-                <View style={{marginLeft: 10, marginRight: 10, marginTop: 5, width: 5, height: 5, borderRadius: 2, backgroundColor: 'grey'}}/>
-                <Text style={{fontSize: 18}}>{
+                <View style={details.dot}/>
+                <Text style={details.baseFont}>{
                   categories[0] ? categories[0].title : ''}
                 </Text>
               </View>
             </View>
-            <Text
-              style={{color: 'white',
-                height: 30,
-                borderRadius: 5,
-                padding: 5,
-                backgroundColor: 'orange',
-                fontSize: 15,
-                fontWeight: 'bold'}}>{`${(distance / 1000).toFixed(1)} km`}</Text>
+            <Text style={details.distance}>
+              {`${(distance / 1000).toFixed(1)} km`}
+            </Text>
           </View>
           <Divider />
-          <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 20}}>
-            <TouchAble>
-              <View style={{alignItems: 'center'}}>
+          <View style={details.secondRow}>
+            <TouchAble onPress={() => openLink(`tel:${phone}`)}>
+              <View style={centerAlign}>
                 <Icon name='md-call' size={35} color='grey' />
-                <Text style={{fontSize: 18}}>{'Call'}</Text>
+                <Text style={details.baseFont}>{'Call'}</Text>
               </View>
             </TouchAble>
-            <TouchAble>
-              <View style={{alignItems: 'center'}}>
+            <TouchAble onPress={() => openLink(url)}>
+              <View style={centerAlign}>
                 <Icon name='md-globe' size={35} color='grey' />
-                <Text style={{fontSize: 18}}>{'Website'}</Text>
+                <Text style={details.baseFont}>{'Website'}</Text>
               </View>
             </TouchAble>
-            <TouchAble>
-              <View style={{alignItems: 'center'}}>
+            <TouchAble
+              onPress={
+                () => openLink(
+                  `${Platform.OS === 'ios' ?
+                    `http://maps.apple.com/?ll=${latitude},${longitude}` :
+                    `geo:${latitude},${longitude}`}`
+                )
+              }>
+              <View style={centerAlign}>
                 <MaterialIcon name='directions' size={35} color='grey' />
-                <Text style={{fontSize: 18}}>{'Directions'}</Text>
+                <Text style={details.baseFont}>{'Directions'}</Text>
               </View>
             </TouchAble>
             <TouchAble>
-              <View style={{alignItems: 'center'}}>
+              <View style={centerAlign}>
                 <Icon name='md-chatboxes' size={35} color='grey' />
-                <Text style={{fontSize: 18}}>{'Invite'}</Text>
+                <Text style={details.baseFont}>{'Invite'}</Text>
               </View>
             </TouchAble>
           </View>
           <Divider />
           <TouchAble>
-            <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 20}}>
-              <Image />
-              <Text style={{fontSize: 18, color: '#414041'}}>{'Book a table with Opentable'}</Text>
-              <Icon name='ios-arrow-forward' size={25} color='grey' style={{marginLeft: 'auto'}}/>
+            <View style={details.listItem}>
+              <MaterialIcon
+                name='blank'
+                size={25} color='grey'
+                style={details.listItemImage}
+              />
+              <Text style={details.listItemText}>{'Book a table with Opentable'}</Text>
+              <Icon name='ios-arrow-forward' size={25} color='grey' style={marginLeftAuto}/>
             </View>
           </TouchAble>
           <Divider />
           <TouchAble>
-            <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 20, alignItems: 'center'}}>
-              <Image />
-              <Text style={{fontSize: 18, color: '#414041'}}>{'Request a ride with Uber'}</Text>
-              <Icon name='ios-arrow-forward' size={25} color='grey' style={{marginLeft: 'auto'}}/>
+            <View style={details.listItem}>
+              <MaterialIcon
+                name='blank'
+                size={25} color='grey'
+                style={details.listItemImage}
+              />
+              <Text style={details.listItemText}>{'Request a ride with Uber'}</Text>
+              <Icon name='ios-arrow-forward' size={25} color='grey' style={marginLeftAuto}/>
             </View>
           </TouchAble>
           <Divider />
           <TouchAble>
-            <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 20, alignItems: 'center'}}>
-              <MaterialIcon name='clock-outline' size={25} color='grey' style={{marginRight: 15}}/>
-              <Text style={{fontSize: 18, color: '#414041'}}>{`${is_closed ? 'Closed' : 'Open Now (07-24h)'}`}</Text>
-              <Icon name='ios-arrow-down' size={25} color='grey' style={{marginLeft: 'auto'}}/>
+            <View style={details.listItem}>
+              <MaterialIcon
+                name='clock-outline'
+                size={25} color='grey'
+                style={details.listItemImage}
+              />
+              <Text style={details.listItemText}>
+                {`${is_closed ? 'Closed' : 'Open Now (07-24h)'}`}
+              </Text>
+              <Icon name='ios-arrow-down' size={25} color='grey' style={marginLeftAuto}/>
             </View>
           </TouchAble>
           <Divider />
           <TouchAble>
-            <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 10, alignItems: 'center'}}>
-              <MaterialIcon name='library' size={25} color='grey' style={{marginRight: 15}}/>
-              <Text style={{fontSize: 18, color: '#414041'}}>{'Menu'}</Text>
-              <MaterialIcon name='link' size={25} color='grey' style={{marginLeft: 'auto'}}/>
+            <View style={{...details.listItem, paddingBottom: 10}}>
+              <MaterialIcon name='library' size={25} color='grey' style={details.listItemImage}/>
+              <Text style={details.listItemText}>{'Menu'}</Text>
+              <MaterialIcon name='link' size={25} color='grey' style={marginLeftAuto}/>
             </View>
           </TouchAble>
         </View>
       </ScrollView>
-      <TouchAble>
-        <View>
-          <Divider/>
-          <Text style={{alignSelf: 'center', backgroundColor: 'white', padding: 20, color: 'orange', fontSize: 20, fontWeight: 'bold'}}>
+      <View>
+        <Divider/>
+        <TouchAble>
+          <Text style={details.footerText}>
             {'VIEW OFFERS'}
           </Text>
-        </View>
-      </TouchAble>
+        </TouchAble>
+      </View>
     </View>
   )
+}
+
+Details.propTypes = {
+  data: PropTypes.shape({})
 }
 
 
